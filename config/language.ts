bd2af1b13cbd54b6af25cfd60bca2d199baa08c1
Abigail Briones <abigailbrionesaranda@gmail.com>
2025-12-27 09:47:13 -0500

    create language config

---
+++ config/language.ts
@@ -0,0 +1,7 @@
+export const SUPPORTED_LANGUAGES = [
+  { code: 'en', name: 'English' },
+  { code: 'qu', name: 'Quechua' },
+  { code: 'es', name: 'Spanish' },
+];
+
+export const DEFAULT_LANGUAGE = 'qu';
